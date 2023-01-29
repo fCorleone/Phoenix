6f5a815e005422c9a6b6a631dd46ce17d15050ea
fCorleone <812921791@qq.com>
2023-01-29 12:30:46 +0800

    rename FISCO-BCOS and remove FISCO-BCOS-chaos

--- FISCO-BCOS-chaos/libinitializer/LightNodeInitializer.cpp
+++
@@ -1,1 +0,0 @@
-#include "LightNodeInitializer.h"
--- FISCO-BCOS-chaos/libtask/bcos-task/Coroutine.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#if __APPLE__ && __clang__
-#include <experimental/coroutine>
-namespace CO_STD = std::experimental;
-#else
-#include <coroutine>
-namespace CO_STD = std;
-#endif
